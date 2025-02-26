0de695890da4d8eac7dc6a0a2f1caef8d8c850b5
Brody Lister <brodylister@csu.fullerton.edu>
2025-02-25 19:32:36 -0800

    initial commit for hw2

--- main.cpp
+++ main.cpp
@@ -1,46 +1,129 @@
-#include <iostream>         // istream, ostream
-#include <memory>           // std::unique_ptr
-#include <vector>
-#include <utility>          // move()
-#include <iomanip>          // std::setprecision()
+#include <exception>
+#include <iostream>
+#include <sstream>                                                                    // istringstream
+#include <typeinfo>
 
-#include "GroceryItem.hpp"  // GroceryItem
-/*
-INSTRUCTIONS:
-Implement function main() to use the GroceryItem class:
-  a. Read a grocery item 1 from standard input (std::cin) until end of file 2. For each
-  grocery item read:
-    i. Store the grocery item in a dynamically created object (e.g., new,
-    make_unique, …)
-    ii. Store the grocery item’s pointer in a standard vector
-  b. After you have reached the end of file, write the grocery items 3 to standard
-  output (std::cout) in reverse order using constant reverse iterators
-  c. Be sure to release the dynamically allocated objects before exiting the
-  program
+#include "GroceryItem.hpp"
+#include "GroceryList.hpp"
 
-*/
 
-int main() {
-  std::cout << std::showpoint << std::fixed << std::setprecision( 2 ); // sticky settings
 
-  // going to attempt to implement this with normal pointers (fingers crossed)
-  std::vector<std::unique_ptr<GroceryItem>> cart;
-  GroceryItem               item;
 
-  std::cout << "Welcome to Kroger.  Place grocery items into your shopping cart by entering each item's information. \nEnclose string entries in quotes, separate fields with commas. \nFor example:  \"00016000306707\", \"Betty Crocker\", \"Betty Crocker Double Chocolate Chunk Cookie Mix\", 17.19 \nEnter CTL-Z (Windows) or CTL-D (Linux) to quit.\n\n";
-
-  // read GroceryItems from cin
-  while( std::cout << "Enter UPC, Product Brand, Product Name, and Price:\n", std::cin >> item )
-  {                                                                          // will use GroceryItem's op>> to store cin to item, loop will continue until end of file or invalid input
-    cart.emplace_back( std::make_unique<GroceryItem>( std::move( item ) ) );
-    std::cout << "Item added to shopping cart: " << *cart.back() << "\n\n";
-  }
-
-  // write GroceryItems to cout in reverse order (using std::const_reverse_iterator)
-  std::cout << "Here is an itemized list of items in your shopping cart:\n";
-  for( auto it = cart.crbegin(); it != cart.crend(); ++it )
+namespace
+{
+  void basicScenario()
   {
-    std::cout << **it << '\n';
+    // Let's start a grocery list
+    GroceryList thingsToBuy = { { "milk"     },
+                                { "hot dogs" },
+                                { "eggs"     },
+                                { "bread"    } };
+
+    // Changed my mind, I want to make sure I can get eggs before running out of money so I'm going to move that to the top of my list
+    thingsToBuy.moveToTop( { "eggs" } );
+
+    // Let's see what's on the list so far
+    std::cout << "My Grocery List" << thingsToBuy << "\n\n";
+
+
+
+    // Hmm ..., no.  Need to add a few more things
+    thingsToBuy += { { "bananas" },
+                     { "apples"  } };
+
+
+
+
+    // My roommate also has a grocery list
+    GroceryList roommatesList = { { "potato chips", "Frito Lays" },
+                                  { "potato chips", "Ruffles"    },
+                                  { "beer",         "Bud Lite"   },
+                                  { "eggs"                       },
+                                  { "pretzels"                   } };
+
+    std::cout << "My roommate's Grocery List" << roommatesList << "\n\n";
+
+    // Let's combine the lists (if different) and go shopping
+    if( thingsToBuy != roommatesList ) thingsToBuy += roommatesList;
+
+
+    // Oops, forgot butter.  Let's add that right before bread.
+    thingsToBuy.insert( { "butter", "Lakes 'Ole" }, thingsToBuy.find( { "bread" } ) );
+
+    // And beer, really?  You should be studying!
+    thingsToBuy.remove( { "beer", "Bud Lite" } );
+
+
+    // Now let's see what's on the list
+    std::cout << "Combined Grocery Lists" << thingsToBuy << "\n\n";
+
+
+
+    // Did you get it right?
+    //
+    // I could construct "expectedResults" with the list of grocery items as above, but I want to exercise GroceryList's extraction
+    // operator.  To do that, I could:
+    //   1) read the list of grocery items from standard input and type the data at the keyboard, but that is time consuming and
+    //      error prone.  That's easily handled by creating a text file with grocery item data then redirecting that text file to
+    //      standard input at the command line when launching the program.  But then I would have to separately maintain that file
+    //      and include it in the set of student files.  More importantly, that would prevent you from doing that in the TO-DO
+    //      section below.
+    //   2) open a text file directly using either command line arguments that supply the file's path and name, or hard coding the
+    //      file's path and name.  But that would require using (and understanding) the standard's ifstream interface, which is easy
+    //      enough given sufficient time, but I don't want to go down that rabbit hole in this course.
+    //   3) create an in-memory text file (well, more accurately, an in-memory input stream) and read from there.  This allows me to
+    //      exercise the extraction operator while avoiding the perils described in options 1) and 2) above.
+    // So, as a design decision, let's chose option 3), create an in-memory input stream and read from there.
+    //
+    //                                            UPC | Brand Name   | Product Name   | Price
+    //                                            ----+--------------+----------------+--------
+    std::istringstream expectedResultsStream( R"( "",   "",            "eggs",          0.0
+                                                  "",   "",            "milk",          0.0
+                                                  "",   "",            "hot dogs",      0.0
+                                                  "",   "Lakes 'Ole",  "butter",        0.0
+                                                  "",   "",            "bread",         0.0
+                                                  "",   "",            "bananas",       0.0
+                                                  "",   "",            "apples",        0.0
+                                                  "",   "Frito Lays",  "potato chips",  0.0
+                                                  "",   "Ruffles",     "potato chips",  0.0
+                                                  "",   "",            "pretzels",      0.0 )"   // multi-line raw string literal of grocery items
+                                               );
+
+    GroceryList expectedResults;
+    expectedResultsStream >> expectedResults;
+
+    std::cout << "\nExpected results:" << expectedResults << "\n\n"
+              << "\nActual results:  " << thingsToBuy     << "\n\n"
+              << "\nTest results:    " << ( thingsToBuy != expectedResults ? "FAIL" : "PASS" ) << '\n';
   }
-  return 0;
+}
+
+
+
+
+int main()
+{
+  try
+  {
+    basicScenario();
+
+
+    ///////////////////////// TO-DO (1) //////////////////////////////
+     /// Create, manipulate, and display your own GroceryList object here.  Not looking for anything specific but don't just repeat
+     /// what I've already done above.  Be imaginative and create your own story.  Maybe you're restocking a depleted food bank, or
+     /// preparing for a holiday meal, or catering a banquette, or planning an all night study session, or ...  You *must* use all
+     /// the functions of the GroceryList interface, including the insertion, extraction, and relational operators.  Try inserting
+     /// grocery items from both the top and the bottom of the lists using the enumerated position values TOP and BOTTOM as well as
+     /// indexed offsets.  Remove grocery items from the top, middle, and bottom.  Create, concatenate, rearrange, and compare
+     /// several (more than two) lists. Have some fun with it!  The purpose is to show me you, as a GroceryList class consumer
+     /// (i.e., the client) understand how to *use* the GroceryList.
+
+    /////////////////////// END-TO-DO (1) ////////////////////////////
+  }
+
+  catch( const std::exception & ex )
+  {
+    std::cerr << "Fatal Error:  unhanded exception \"" << typeid( ex ).name() << "\"\n"
+              << ex.what() << '\n';
+  }
 }
